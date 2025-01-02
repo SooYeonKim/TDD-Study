1bd97b6fa7b3a6d8fb6e56ae014e5e32d9e1c62e
SooYeonKim <lovesu5632@gmail.com>
2025-01-02 22:17:31 +0900

    refactor: @BeforeEach 사용하여 반복되는 Mock 객체 생성 정리

--- tdd/src/test/java/com/example/tdd/service/StudentScoreServiceMockTest.java
+++ tdd/src/test/java/com/example/tdd/service/StudentScoreServiceMockTest.java
@@ -10,6 +10,7 @@ import com.example.tdd.repository.StudentFailRepository;
 import com.example.tdd.repository.StudentPassRepository;
 import com.example.tdd.repository.StudentScoreRepository;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
@@ -18,16 +19,27 @@ import org.mockito.Mockito;
 import java.util.List;
 
 public class StudentScoreServiceMockTest {
+    private StudentScoreService studentScoreService;
+    private StudentScoreRepository studentScoreRepository;
+    private StudentPassRepository studentPassRepository;
+    private StudentFailRepository studentFailRepository;
+
+    @BeforeEach
+    public void beforeEach() {
+        studentScoreRepository = Mockito.mock(StudentScoreRepository.class);
+        studentPassRepository = Mockito.mock(StudentPassRepository.class);
+        studentFailRepository = Mockito.mock(StudentFailRepository.class);
+        studentScoreService = new StudentScoreService(
+                studentScoreRepository,
+                studentPassRepository,
+                studentFailRepository
+        );
+    }
 
     @Test
     @DisplayName("첫번째 Mock 테스트")
     public void firstSaveScoreMockTest() {
         // given
-        StudentScoreService studentScoreService = new StudentScoreService(
-                Mockito.mock(StudentScoreRepository.class),
-                Mockito.mock(StudentPassRepository.class),
-                Mockito.mock(StudentFailRepository.class)
-        );
         String givenStudentName = "StudentA";
         String givenExam = "testexam";
         Integer givenKorScore = 80;
@@ -48,15 +60,6 @@ public class StudentScoreServiceMockTest {
     @DisplayName("성적 저장 로직 검증 / 60점 이상인 경우")
     public void saveScoreMockTest() {
         // given : 평균점수가 60점 이상인 경우
-        StudentScoreRepository studentScoreRepository = Mockito.mock(StudentScoreRepository.class);
-        StudentPassRepository studentPassRepository = Mockito.mock(StudentPassRepository.class);
-        StudentFailRepository studentFailRepository = Mockito.mock(StudentFailRepository.class);
-
-        StudentScoreService studentScoreService = new StudentScoreService(
-                studentScoreRepository,
-                studentPassRepository,
-                studentFailRepository
-        );
         String givenStudentName = "StudentA";
         String givenExam = "testexam";
         Integer givenKorScore = 80;
@@ -118,15 +121,6 @@ public class StudentScoreServiceMockTest {
     @DisplayName("성적 저장 로직 검증 / 60점 미만인 경우")
     public void saveScoreMockTest2() {
         // given : 평균점수가 60점 미만인 경우
-        StudentScoreRepository studentScoreRepository = Mockito.mock(StudentScoreRepository.class);
-        StudentPassRepository studentPassRepository = Mockito.mock(StudentPassRepository.class);
-        StudentFailRepository studentFailRepository = Mockito.mock(StudentFailRepository.class);
-
-        StudentScoreService studentScoreService = new StudentScoreService(
-                studentScoreRepository,
-                studentPassRepository,
-                studentFailRepository
-        );
         String givenStudentName = "StudentA";
         String givenExam = "testexam";
         Integer givenKorScore = 40;
@@ -186,10 +180,6 @@ public class StudentScoreServiceMockTest {
     @DisplayName("합격자 명단 가져오기 검증")
     public void getPassStudentsListTest() {
         // given : 평균점수가 60점 이상인 경우
-        StudentScoreRepository studentScoreRepository = Mockito.mock(StudentScoreRepository.class);
-        StudentPassRepository studentPassRepository = Mockito.mock(StudentPassRepository.class);
-        StudentFailRepository studentFailRepository = Mockito.mock(StudentFailRepository.class);
-
         StudentPass expectStudent1 = StudentPass.builder().id(1L).studentName("StudentA").exam("testexam").avgScore(70.0).build();
         StudentPass expectStudent2 = StudentPass.builder().id(2L).studentName("StudentB").exam("testexam").avgScore(80.0).build();
         StudentPass notExpectStudent3 = StudentPass.builder().id(3L).studentName("StudentC").exam("secondexam").avgScore(90.0).build();
@@ -200,11 +190,6 @@ public class StudentScoreServiceMockTest {
                 notExpectStudent3
         ));
 
-        StudentScoreService studentScoreService = new StudentScoreService(
-                studentScoreRepository,
-                studentPassRepository,
-                studentFailRepository
-        );
         String givenTestExam = "testexam";
 
         // when
@@ -222,10 +207,6 @@ public class StudentScoreServiceMockTest {
     @DisplayName("불합격자 명단 가져오기 검증")
     public void getFailStudentsListTest() {
         // given : 평균점수가 60점 미만인 경우
-        StudentScoreRepository studentScoreRepository = Mockito.mock(StudentScoreRepository.class);
-        StudentPassRepository studentPassRepository = Mockito.mock(StudentPassRepository.class);
-        StudentFailRepository studentFailRepository = Mockito.mock(StudentFailRepository.class);
-
         StudentFail expectStudent1 = StudentFail.builder().id(1L).studentName("StudentA").exam("testexam").avgScore(50.0).build();
         StudentFail expectStudent2 = StudentFail.builder().id(2L).studentName("StudentB").exam("testexam").avgScore(45.0).build();
         StudentFail notExpectStudent3 = StudentFail.builder().id(3L).studentName("StudentC").exam("secondexam").avgScore(35.0).build();
@@ -235,12 +216,7 @@ public class StudentScoreServiceMockTest {
                 expectStudent2,
                 notExpectStudent3
         ));
-
-        StudentScoreService studentScoreService = new StudentScoreService(
-                studentScoreRepository,
-                studentPassRepository,
-                studentFailRepository
-        );
+        
         String givenTestExam = "testexam";
 
         // when
