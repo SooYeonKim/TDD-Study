cb720d08b8207616f83881119848c472591e1b17
SooYeonKim <lovesu5632@gmail.com>
2025-01-08 20:42:34 +0900

    test: Archunit 컨벤션 테스트 작성

--- test/src/main/java/com/example/test/config/S3Config.java
+++ test/src/main/java/com/example/test/config/S3Config.java
@@ -1,4 +1,4 @@
-package com.example.test.service;
+package com.example.test.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
